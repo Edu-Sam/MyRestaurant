e454dcd204de6ac48527e8328041a99c00b7cd23
Edu-Sam <edungugi20@gmail.com>
2021-09-20 15:21:31 +0300

    Initial Commit

---
+++ MyRestaurant/MyRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRestaurantApp.swift
+//  MyRestaurant
+//
+//  Created by Admin on 20.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
